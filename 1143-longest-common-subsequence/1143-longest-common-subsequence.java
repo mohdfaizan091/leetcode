cbda0fefb70e4b521a51e6a64e6624b927996126
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-07-04 16:55:58 +0530

    Time: 21 ms (53.36%), Space: 55.4 MB (5.86%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -2,36 +2,37 @@ class Solution {
     static int[][] dp;
 
     public int longestCommonSubsequence(String text1, String text2) {
-        StringBuilder sb1 = new StringBuilder(text1);
-        StringBuilder sb2 = new StringBuilder(text2);
 
-        int m = sb1.length();
-        int n = sb2.length();
+        int m = text1.length();
+        int n = text2.length();
 
-        dp = new int[m][n];
+        dp = new int[m + 1][n + 1];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j] = -1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
-
-        return CommonSubsequence(sb1, sb2, m - 1, n - 1);
+        return dp[m][n];
     }
 
-    public int CommonSubsequence(StringBuilder text1, StringBuilder text2, int idx1, int idx2) {
-        if (idx1 < 0 || idx2 < 0)
-            return 0;
+    // public int CommonSubsequence(StringBuilder text1, StringBuilder text2, int idx1, int idx2) {
+    //     if (idx1 < 0 || idx2 < 0)
+    //         return 0;
 
-        if (dp[idx1][idx2] != -1)
-            return dp[idx1][idx2];
+    //     if (dp[idx1][idx2] != -1)
+    //         return dp[idx1][idx2];
 
-        if (text1.charAt(idx1) == text2.charAt(idx2)) {
-            return dp[idx1][idx2] = 1 + CommonSubsequence(text1, text2, idx1 - 1, idx2 - 1);
-        }
+    //     if (text1.charAt(idx1) == text2.charAt(idx2)) {
+    //         return dp[idx1][idx2] = 1 + CommonSubsequence(text1, text2, idx1 - 1, idx2 - 1);
+    //     }
 
-        return dp[idx1][idx2] = Math.max(
-                CommonSubsequence(text1, text2, idx1 - 1, idx2),
-                CommonSubsequence(text1, text2, idx1, idx2 - 1));
-    }
+    //     return dp[idx1][idx2] = Math.max(
+    //             CommonSubsequence(text1, text2, idx1 - 1, idx2),
+    //             CommonSubsequence(text1, text2, idx1, idx2 - 1));
+    // }
 }
