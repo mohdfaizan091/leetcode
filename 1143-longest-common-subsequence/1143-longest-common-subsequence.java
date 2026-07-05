380bf86d0c483e2a99d2ad2cbf55503fbd37c420
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-07-05 13:14:38 +0530

    Refactor LCS implementation with memoization
    Refactored longestCommonSubsequence method to include recursion with memoization. Improved printLCS method for better readability and functionality.

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -2,7 +2,7 @@ class Solution {
     static int[][] dp;
 
     public int longestCommonSubsequence(String text1, String text2) {
-
+// tabulation
         int m = text1.length();
         int n = text2.length();
 
@@ -20,46 +20,55 @@ class Solution {
         printLCS(dp, text1, text2);
         return dp[m][n];
     }
+// recursion + memoaization
+    public int CommonSubsequence(StringBuilder text1, StringBuilder text2, int idx1, int idx2) {
+        if (idx1 < 0 || idx2 < 0)
+            return 0;
 
-    // public int CommonSubsequence(StringBuilder text1, StringBuilder text2, int idx1, int idx2) {
-    //     if (idx1 < 0 || idx2 < 0)
-    //         return 0;
+        if (dp[idx1][idx2] != -1)
+            return dp[idx1][idx2];
 
-    //     if (dp[idx1][idx2] != -1)
-    //         return dp[idx1][idx2];
+        if (text1.charAt(idx1) == text2.charAt(idx2)) {
+            return dp[idx1][idx2] = 1 + CommonSubsequence(text1, text2, idx1 - 1, idx2 - 1);
+        }
 
-    //     if (text1.charAt(idx1) == text2.charAt(idx2)) {
-    //         return dp[idx1][idx2] = 1 + CommonSubsequence(text1, text2, idx1 - 1, idx2 - 1);
-    //     }
-
-    //     return dp[idx1][idx2] = Math.max(
-    //             CommonSubsequence(text1, text2, idx1 - 1, idx2),
-    //             CommonSubsequence(text1, text2, idx1, idx2 - 1));
-    // }
+        return dp[idx1][idx2] = Math.max(
+                CommonSubsequence(text1, text2, idx1 - 1, idx2),
+                CommonSubsequence(text1, text2, idx1, idx2 - 1));
+    }
 
     //print lcs
     public void printLCS(int[][] dp, String text1, String text2) {
 
-    int m = text1.length();
-    int n = text2.length();
+        int m = text1.length();
+        int n = text2.length();
 
-    StringBuilder sb = new StringBuilder();
-
-    while (m > 0 && n > 0) {
-
-        if (text1.charAt(m - 1) == text2.charAt(n - 1)) {
-            sb.append(text1.charAt(m - 1));
-            m--;
-            n--;
+        StringBuilder sb = new StringBuilder();
+    
+        while (m > 0 && n > 0) {
+    
+            if (text1.charAt(m - 1) == text2.charAt(n - 1)) {
+                sb.append(text1.charAt(m - 1));
+                m--;
+                n--;
+            }
+            else if (dp[m - 1][n] >= dp[m][n - 1]) {
+                m--;
+            }
+            else {
+                n--;
+            }
         }
-        else if (dp[m - 1][n] >= dp[m][n - 1]) {
-            m--;
-        }
-        else {
-            n--;
-        }
+        
+        System.out.println(sb.reverse().toString());
     }
+}
 
-    System.out.println(sb.reverse().toString());
-}
-}
+text1 = "abcde"
+text2 = "ace"
+Stdout = ace
+
+Output
+3
+Expected
+3
