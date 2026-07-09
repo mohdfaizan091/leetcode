36867e33212f909f880b12f7b42d274ef437b447
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-07-09 21:32:44 +0530

    Time: 17 ms (34.37%), Space: 53.1 MB (6.07%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,9 +1,48 @@
 class Solution {
     public int countSubstrings(String s) {
-        //bruteforce - Approach 1
-        return Substring(s);
+        return Sub_string(s);
     }
+    public int Sub_string(String s) {
+    int n = s.length();
+    int[][] dp = new int[n][n];
+
+    // length-1 substrings are always palindromes
+    for (int i = 0; i < n; i++) {
+        dp[i][i] = 1;
+    }
+    int j = 1;
+    while (j < n) {
+        int p = 0;
+        int q = j;
+        while (q < n) {
+            if (s.charAt(p) != s.charAt(q)) {
+                dp[p][q] = 0;
+            } else {
+                if (q - p == 1) {
+                    dp[p][q] = 1;
+                } else if (dp[p + 1][q - 1] == 1) {
+                    dp[p][q] = 1;
+                } else {
+                    dp[p][q] = 0;
+                }
+            }
+            p++;
+            q++;
+        }
+        j++; 
+    }
+
+    int count = 0;
+    for (int l = 0; l < n; l++) {
+        for (int m = 0; m < n; m++) {
+            if (dp[l][m] == 1)
+                count++;
+        }
+    }
+    return count;
+}
     public int Substring(String s) {
+        //bruteforce - Approach 1
         int count = 0;
         for(int i=0;i<s.length();i++) {
             for(int j=i;j<s.length();j++) {
