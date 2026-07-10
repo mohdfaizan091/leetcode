d67bcaa250955f1bceae0a8b4da7119062836c93
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-07-10 22:39:59 +0530

    Time: 5 ms (12.9%), Space: 48.2 MB (5.59%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,17 +1,25 @@
 
 class Solution {
+    static HashMap<TreeNode, Integer> dp = new HashMap<>();
     public int levels(TreeNode root) {
-        if(root == null) return 0;
+        if (root == null) return 0;
+        if (dp.containsKey(root)) return dp.get(root);
         int left = levels(root.left);
         int right = levels(root.right);
-        return 1 + Math.max(left, right);
+        int height = 1 + Math.max(left, right);
+        dp.put(root, height);
+        return height;
     }
-    public int diameterOfBinaryTree(TreeNode root) {
+    public int diameter(TreeNode root) {
         if(root == null) return 0;
         int dia = levels(root.left) + levels(root.right);
-        int left = diameterOfBinaryTree(root.left);
-        int right = diameterOfBinaryTree(root.right);
+        int left = diameter(root.left);
+        int right = diameter(root.right);
         return Math.max(dia, Math.max(left, right));
+
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        return diameter(root);
 
     }
 }
