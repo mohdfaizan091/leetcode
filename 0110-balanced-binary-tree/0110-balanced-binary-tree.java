de99935ecb52f305a168e232456ba2bcad89963b
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-07-12 00:05:38 +0530

    Time: 0 ms (100%), Space: 45.8 MB (22.98%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -1,17 +1,18 @@
 
 class Solution {
+    static boolean flag;
     public int levels(TreeNode root) {
         if(root == null) return 0;
         int left = levels(root.left);
         int right = levels(root.right);
+        if(Math.abs(left - right)>1){
+            flag = false;
+        }
         return 1 + Math.max(left, right);
     }
     public boolean isBalanced(TreeNode root) {
-        if(root == null) return true;
-        if(Math.abs(levels(root.left)-levels(root.right))>1){
-            return false;
-        }
-        return (isBalanced(root.left) && isBalanced(root.right));
-
+        flag = true;
+        levels(root);
+        return flag;
     }
 }
