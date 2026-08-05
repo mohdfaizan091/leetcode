4bf2f24e1edb61c71c4eb21a3283d47f6354eb4d
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-08-05 23:26:22 +0530

    Time: 6 ms (8.14%), Space: 44 MB (61.9%) - LeetHub

--- 0781-rabbits-in-forest/0781-rabbits-in-forest.java
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.java
@@ -4,18 +4,24 @@ class Solution {
         int samecolor = 0;
         Arrays.sort(answers);
         for(int i=0 ; i<answers.length ; i++) {
+            // first should be count
             if(i==0) {
                 count += answers[i] + 1;
+                // to track how many have same color
                 samecolor = answers[i];
             }
+            // if rat say '0' then he is unique and should be count
             else if(answers[i] == 0){
                 count++;
             } 
             else {
+                // if same color meet again then dont count;
                 if(answers[i-1] == answers[i] && samecolor != 0) {
                     samecolor--;
                 } else {
+                    // saw different color of rat and count it
                     count += answers[i] + 1;
+                    // make track of that color
                     samecolor = answers[i];
                 }
             }
