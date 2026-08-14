a95964b2d24ab11933739a436dd12f377df0ccb8
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-08-14 19:03:32 +0530

    Time: 2 ms (33.23%), Space: 45.8 MB (15.48%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,25 +1,25 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> permutation = new ArrayList<>();
-        backTrack(ans , permutation , nums);
-        return ans;
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        computeNoOfPermutation(nums , result , temp);
+        return result;
     }
 
-    public void backTrack(List<List<Integer>> ans , List<Integer> permutation , int[] nums) {
-        if(permutation.size() == nums.length) {
-            ans.add(new ArrayList<>(permutation));
+    public void computeNoOfPermutation(int[] nums , List<List<Integer>> result , List<Integer> temp) {
+        if(temp.size() == nums.length) {
+            // result;
+            result.add(new ArrayList<>(temp));
             return;
         }
-
         for(int i=0 ; i<nums.length ; i++) {
-            if(permutation.contains(nums[i])) {
+            if(temp.contains(nums[i])) {
+                // to check that is no already present in in permutation
                 continue;
             }
-
-            permutation.add(nums[i]);
-            backTrack(ans , permutation , nums);
-            permutation.remove(permutation.size()-1);
+            temp.add(nums[i]);
+            computeNoOfPermutation(nums , result , temp);
+            temp.remove(temp.size()-1);
         }
     }
 }
