e8e47d7e17acc4f6c8288b0d702bcb63038bab44
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-08-16 00:42:33 +0530

    Time: 0 ms (100%), Space: 42.5 MB (29.02%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,5 +1,9 @@
 class Solution {
     public int hammingWeight(int n) {
+        return noOfOnes2(n);
+    }
+    // approach 1 : bruteforce TC : O(32);
+    public int noOfOnes1(int n) {
         int count = 0;
         while(n != 0) {
             if(n % 2 != 0) count++;
@@ -7,4 +11,14 @@ class Solution {
         }
         return count;
     }
+    // approach 2 : bruteforce TC : O(count);
+    public int noOfOnes2(int n) {
+        int count = 0;
+        while(n != 0) {
+            n = (n & (n-1));
+            count++;
+        }
+        return count;
+    }
+
 }
