2488ce5b93ee305cc676a92ab0d36ed7466a0e7f
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-08-15 23:13:16 +0530

    Time: 1 ms (96.61%), Space: 42 MB (99.16%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,10 +1,22 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
+        // return isPowerOfTwo1(n);
+        return isPowerOfTwo2(n);
+    }
+    // approach 1 : bruteforce  TC = O(log(n))
+    public boolean isPowerOfTwo1(int n) {
         if(n==0) return false;
         while(n%2 == 0) {
             n = n / 2;
         }
         if(n==1) return true;
+        return false;
+    }
+
+    // approach 1 : TC = O(1)
+    public boolean isPowerOfTwo2(int n) {
+        if(n<=0) return false;
+        if((n & (n-1)) == 0) return true;
         return false;
     }
 }
