5e9965df3584bc562d6d2dd4887fd06355aaf259
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-08-17 00:24:36 +0530

    Time: 1 ms (100%), Space: 48.1 MB (23.02%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,8 +1,9 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
         int result[] = new int[2];
-        return singleNo1(nums , result);
+        return singleNo2(nums , result);
     }
+    // brute-force approach  TC = O(n^2)
     public int[] singleNo1(int[] nums , int[] result) {
         int k=0;
         for(int i=0 ; i<nums.length ; i++) {
@@ -19,4 +20,25 @@ class Solution {
         }
         return result;
     }
+    // approach 02  : TC = O(n)
+    public int[] singleNo2(int[] nums , int[] ans) {
+        int xor = 0;
+        for(int ele : nums) {
+            xor ^= ele;
+        }
+        int mask = xor & xor - 1;
+        mask = mask ^ xor;
+        int b1 = 0 , b2 = 0;
+        for(int ele : nums) {
+            if((ele & mask) == 0) {
+                b1 ^= ele;
+            } else {
+                b2 ^= ele;
+            }
+        }
+        ans[0] = b1;
+        ans[1] = b2;
+        return ans;
+    }
+    
 }
