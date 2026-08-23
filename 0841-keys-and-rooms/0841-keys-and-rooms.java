ebb3ed743f744098128d82fbc115e407480c1873
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-08-23 17:45:41 +0530

    Time: 0 ms (100%), Space: 46.2 MB (81.55%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,16 +1,29 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean dfs = depthFirstSearch(rooms);
+        return dfs;
+    }
+    public boolean depthFirstSearch(List<List<Integer>> rooms) {
         int n = rooms.size();
         boolean[] visited = new boolean[n];
 
         //breeadth first search
-        bfs(rooms , visited , 0);
+        dfs(rooms , visited , 0);
         
         // check if any rooms is unvisited
         for(int i=0 ; i<n ; i++) {
             if(!visited[i]) return false;
         }
         return true;
+    } 
+    public void dfs(List<List<Integer>> rooms , boolean[] visited , int idx) {
+        visited[idx] = true;
+        List<Integer> list = rooms.get(idx);
+        for(int i=0 ; i<list.size() ; i++) {
+            if(!visited[list.get(i)]) {
+                dfs(rooms , visited , list.get(i));
+            }
+        }
     }
     public void bfs(List<List<Integer>> rooms , boolean[] visited , int key) {
         Queue<Integer> q = new LinkedList<>();
