f308d5db975aa323ae722c25b7a0e33c5686f85f
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-08-26 18:35:10 +0530

    Time: 4 ms (91.37%), Space: 47.2 MB (48.46%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -5,34 +5,68 @@ class Solution {
             adjacencyList.add(new ArrayList<>());
         }
         List<Integer> order = new ArrayList<>();
-        int[] indegree = new int[n];
-        makeList(adjacencyList , edges , indegree);
-        // kahn's algorithm
-        Queue<Integer> q = new LinkedList<>();
+        makeList(adjacencyList , edges);
+
+
+        // kahn's algorithm--------------------------------------------------------
+
+        // Queue<Integer> q = new LinkedList<>();
+        // for(int i=0 ; i<n ; i++) {
+        //     if(indegree[i] == 0) {
+        //         q.add(i);
+        //     }
+        // }
+        // while(q.size() > 0) {
+        //     int front = q.remove();
+        //     for(int ele : adjacencyList.get(front)) {
+        //         indegree[ele]--;
+        //         if(indegree[ele] == 0) {
+        //             q.add(ele);
+        //         }
+        //     }
+        //     order.add(front);
+        // }
+        // return (n == order.size());      
+
+// --------------------------------------------------------------------------------------
+
+
+        // dfs
+
+        boolean[] isvis = new boolean[n];
+        boolean[] path = new boolean[n];
         for(int i=0 ; i<n ; i++) {
-            if(indegree[i] == 0) {
-                q.add(i);
-            }
-        }
-        while(q.size() > 0) {
-            int front = q.remove();
-            for(int ele : adjacencyList.get(front)) {
-                indegree[ele]--;
-                if(indegree[ele] == 0) {
-                    q.add(ele);
+            if(!isvis[i]) {
+                if(dfs(adjacencyList, isvis, path, i)) {
+                    return false;
                 }
             }
-            order.add(front);
         }
-        return (n == order.size());        
+        return true;
     }
 
-    public void makeList(List<List<Integer>> adjacencyList , int[][] edges , int[] indegree) {
+    public boolean dfs(List<List<Integer>> adjList , boolean[] isvis , boolean[] path , int idx) {
+        isvis[idx] = true;
+        path[idx] = true;
+        for(int ele : adjList.get(idx)) {
+            if(!isvis[ele]) {
+                if(dfs(adjList, isvis, path, ele)) {
+                    return true;
+                }
+            }
+            else if(path[ele]) return true;
+        }
+        path[idx] = false;
+        return false;
+    }
+
+    // ----------------------
+
+    public void makeList(List<List<Integer>> adjacencyList , int[][] edges) {
         for(int i=0 ; i<edges.length ; i++) {
             int u = edges[i][0];
             int v = edges[i][1];
             adjacencyList.get(v).add(u);
-            indegree[u]++;
         }
     }
 }
