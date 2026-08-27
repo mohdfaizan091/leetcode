3e60b1f02e388b5acf63dfd118d09967e1662e4a
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-08-27 12:50:43 +0530

    Time: 10 ms (81.03%), Space: 49 MB (57.72%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,5 +1,6 @@
 class Solution {
-    class pair implements java.util.Comparator<pair> {
+// pair --
+    public class pair implements Comparable<pair> {
         int node;
         int time;
 
@@ -9,19 +10,20 @@ class Solution {
         }
 
         @Override
-        public int compare(pair p1, pair p2) {
-            return Integer.compare(p1.time, p2.time);
+        public int compareTo(pair p) {
+            if(this.time == p.time) return this.node - p.node;
+            return this.time - p.time;
         }
     }
-
+// --
     public int networkDelayTime(int[][] times, int n, int src) {
         List<List<pair>> adj = new ArrayList<>();
-
+// create adj list
         for(int i = 0; i <=n; i++) {
             adj.add(new ArrayList<pair>());
         }
 
-        // fill the pair list
+// fill the pair list
         for(int i = 0; i < times.length; i++) {
             int u = times[i][0];
             int v = times[i][1];
@@ -29,16 +31,15 @@ class Solution {
 
             adj.get(u).add(new pair(v, weight));
         }
-
+// ans array
         int[] minNetworkTime = new int[n + 1];
         Arrays.fill(minNetworkTime, Integer.MAX_VALUE);
-
+// starting or source always be '0'
         minNetworkTime[src] = 0;
 
-        PriorityQueue<pair> pq = new PriorityQueue<>(new pair(0, 0));
-
+// Dijkstra Algorithm ------------------
+        PriorityQueue<pair> pq = new PriorityQueue<pair>();
         pq.add(new pair(src, 0));
-
         while(pq.size() > 0) {
             pair front = pq.remove();
 
@@ -54,9 +55,10 @@ class Solution {
                 }
             }
         }
+// ----------------------------------------
 
+// finding network delay time
         int max = -1;
-
         for(int i = 1; i <= n; i++) {
             if(minNetworkTime[i] == Integer.MAX_VALUE)
                 return -1;
