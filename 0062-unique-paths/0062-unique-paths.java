1515043b63e7bea1ea17a049261d58138b0c2f45
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-09-20 13:27:23 +0530

    Time: 0 ms (100%), Space: 42.2 MB (28.57%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i=0 ; i<m ; i++) {
+            for(int j=0 ; j<n ; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return countUniquePath(0 , 0 , m , n , dp);
+    }
+    public int countUniquePath(int i , int j , int m , int n , int[][] dp) {
+        if(i == m-1 || j == n-1) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = countUniquePath(i + 1 , j , m , n , dp) + countUniquePath(i , j + 1 , m , n , dp);
+    }
+}
