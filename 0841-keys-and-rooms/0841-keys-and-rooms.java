43a6848ac6b183e786b15b36f6d153ca12475506
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-09-21 12:35:49 +0530

    Time: 4 ms (7.94%), Space: 45.5 MB (99.67%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,50 +1,21 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        return canVisitAllRoom(rooms);
-    }
-    
-    public boolean canVisitAllRoom(List<List<Integer>> rooms) {
         int n = rooms.size();
-        boolean[] visited = new boolean[n];
-
-        dfs(rooms , visited , 0);
-        // bfs(rooms , visited , 0);
-        
-        // check if any rooms is unvisited
-        for(int i=0 ; i<n ; i++) {
-            if(!visited[i]) return false;
-        }
-        return true;
-    } 
-    
-
-    // dfs
-    public void dfs(List<List<Integer>> rooms , boolean[] visited , int idx) {
-        visited[idx] = true;
-        List<Integer> list = rooms.get(idx);
-        for(int i=0 ; i<list.size() ; i++) {
-            if(!visited[list.get(i)]) {
-                dfs(rooms , visited , list.get(i));
+        boolean[] isvis = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        while(q.size() > 0) {
+            int top = q.remove();
+            isvis[top] = true;
+            for(int ele : rooms.get(top)) {
+                if(!isvis[ele]) {
+                    q.add(ele);
+                }
             }
         }
-    }
-
-
-    // bfs
-    public void bfs(List<List<Integer>> rooms , boolean[] visited , int key) {
-        Queue<Integer> q = new LinkedList<>();
-
-        q.add(key);
-        visited[key] = true;
-
-        while (!q.isEmpty()) { 
-        int currentRoom = q.remove(); 
-            for (int neighbor : rooms.get(currentRoom)) { 
-                if (!visited[neighbor]) { 
-                    q.add(neighbor); 
-                    visited[neighbor] = true; 
-                } 
-            } 
+        for(int i=0 ; i<n ; i++) {
+            if(!isvis[i]) return false;
         }
+        return true;
     }
 }
